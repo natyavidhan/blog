e569f25245ba27303ee5063dd8af8f1c52434c64
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-15 00:45:38 +0530

    header links

--- src/components/head.js
+++ src/components/head.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function head() {
   return (    <div className="header text-center">
     <h1>Natya's Blog</h1>
     <h5>Stuff that fascinates me</h5>
     <div className="buttons mt-3">
-        <button className="header-button">Home</button>
-        <button className="header-button">Resume</button>
-        <button className="header-button">LinkedIn</button>
-        <button className="header-button">Github</button>
+        <Link to={"/"}><button className="header-button">Home</button></Link>
+        <button className="header-button" onClick={()=>{window.open('https://natya.is-a.dev')}}>Resume</button>
+        <button className="header-button" onClick={()=>{window.open('https://www.linkedin.com/in/natyavidhan')}}>LinkedIn</button>
+        <button className="header-button" onClick={()=>{window.open('https://github.com/natyavidhan')}}>Github</button>
     </div>
 </div>
   )
