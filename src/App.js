80c5d9540891a1d9d1fcf69dc515e4b95074ff32
studiousgamer <natyavidhanbiswas10@gmail.com>
2024-07-15 01:44:41 +0530

    more meta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Blogbox from "./components/blogbox";
 import PropTypes from 'prop-types'
+import {Helmet} from "react-helmet";
 import Head from "./components/head";
 function App(props) {
 	return (
+		<>
+        <Helmet>
+            <title>Natya's blog</title>
+            <meta name="description" content="My personal blog for posting stuff" />
+        </Helmet>
 		<div className="container mt-5">
 			<Head />
 			<div className="blogs mt-5">
@@ -15,6 +21,7 @@ function App(props) {
 				})}
 			</div>
 		</div>
+		</>
 	);
 }
 
--- src/Blog.js
+++ src/Blog.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import $ from 'jquery';
+import {Helmet} from "react-helmet";
+
 import Head from './components/head';
+
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import remarkImages from 'remark-images'
@@ -11,20 +14,28 @@ function Blog() {
     const { date } = useParams();
 
     const [md, setmd] = useState(0)
+    const [title, settitle] = useState("")
 
     $.ajax({
         type: 'GET',
         url: `blogs/${date}.md`,
         success: (msg) => {
             setmd(msg)
+            settitle(msg.split("\n")[0].replace("#", "") + " | ")
         }
     })
 
     return (
+        <>
+        <Helmet>
+            <title>{`${title}${date}`}</title>
+            <meta name="description" content="open the blog to see content" />
+        </Helmet>
         <div className="container mt-5">
             <Head/>
             <Markdown className='mt-5' remarkPlugins={[remarkGfm, remarkImages]}>{md}</Markdown>
         </div>
+        </>
     )
 }
 
